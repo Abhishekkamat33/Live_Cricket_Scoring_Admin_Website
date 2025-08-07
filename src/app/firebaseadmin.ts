c656b5ff1bb447c22bb0b16daa3ac0161cf284bf
abhishek kamat <abhishekkamat33@gmail.com>
2025-08-07 15:10:40 +0545

    Finnaly deployed2

--- src/app/firebaseadmin.ts
+++ src/app/firebaseadmin.ts
@@ -1,17 +1,14 @@
 import admin from 'firebase-admin';
 
-// Use a global variable to ensure admin is only initialized once in development
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      // Use private key with proper line breaks replaced:
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    // Optionally specify databaseURL if you use Realtime Database
-    // databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });
 }
-
 export default admin;
+
